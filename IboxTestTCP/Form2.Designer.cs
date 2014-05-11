bd545e33566b2a62f3576390eecf78085e1462e2
bklemencz <bklemencz@gmail.com>
2014-05-11 23:58:12 +0800

    ZedGraph Resources

---
+++ IboxTestTCP/Form2.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace IboxTestTCP
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.GrVarListCheckBox = new System.Windows.Forms.CheckedListBox();
+            this.CloseBtn = new System.Windows.Forms.Button();
+            this.StrtBtn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1022, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.IsAutoScrollRange = false;
+            this.zedGraphControl1.IsEnableHPan = true;
+            this.zedGraphControl1.IsEnableHZoom = true;
+            this.zedGraphControl1.IsEnableVPan = true;
+            this.zedGraphControl1.IsEnableVZoom = true;
+            this.zedGraphControl1.IsScrollY2 = false;
+            this.zedGraphControl1.IsShowContextMenu = true;
+            this.zedGraphControl1.IsShowCursorValues = false;
+            this.zedGraphControl1.IsShowHScrollBar = false;
+            this.zedGraphControl1.IsShowPointValues = false;
+            this.zedGraphControl1.IsShowVScrollBar = false;
+            this.zedGraphControl1.IsZoomOnMouseCenter = false;
+            this.zedGraphControl1.Location = new System.Drawing.Point(5, 5);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.PanButtons = System.Windows.Forms.MouseButtons.Left;
+            this.zedGraphControl1.PanButtons2 = System.Windows.Forms.MouseButtons.Middle;
+            this.zedGraphControl1.PanModifierKeys2 = System.Windows.Forms.Keys.None;
+            this.zedGraphControl1.PointDateFormat = "g";
+            this.zedGraphControl1.PointValueFormat = "G";
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(1011, 443);
+            this.zedGraphControl1.TabIndex = 1;
+            this.zedGraphControl1.ZoomButtons = System.Windows.Forms.MouseButtons.Left;
+            this.zedGraphControl1.ZoomButtons2 = System.Windows.Forms.MouseButtons.None;
+            this.zedGraphControl1.ZoomModifierKeys = System.Windows.Forms.Keys.None;
+            this.zedGraphControl1.ZoomModifierKeys2 = System.Windows.Forms.Keys.None;
+            this.zedGraphControl1.ZoomStepFraction = 0.1D;
+            // 
+            // GrVarListCheckBox
+            // 
+            this.GrVarListCheckBox.FormattingEnabled = true;
+            this.GrVarListCheckBox.Location = new System.Drawing.Point(1022, 5);
+            this.GrVarListCheckBox.Name = "GrVarListCheckBox";
+            this.GrVarListCheckBox.Size = new System.Drawing.Size(188, 364);
+            this.GrVarListCheckBox.TabIndex = 2;
+            // 
+            // CloseBtn
+            // 
+            this.CloseBtn.Location = new System.Drawing.Point(1117, 404);
+            this.CloseBtn.Name = "CloseBtn";
+            this.CloseBtn.Size = new System.Drawing.Size(93, 23);
+            this.CloseBtn.TabIndex = 3;
+            this.CloseBtn.Text = "Close";
+            this.CloseBtn.UseVisualStyleBackColor = true;
+            this.CloseBtn.Click += new System.EventHandler(this.CloseBtn_Click);
+            // 
+            // StrtBtn
+            // 
+            this.StrtBtn.Location = new System.Drawing.Point(1022, 404);
+            this.StrtBtn.Name = "StrtBtn";
+            this.StrtBtn.Size = new System.Drawing.Size(89, 23);
+            this.StrtBtn.TabIndex = 4;
+            this.StrtBtn.Text = "Start";
+            this.StrtBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1211, 454);
+            this.ControlBox = false;
+            this.Controls.Add(this.StrtBtn);
+            this.Controls.Add(this.CloseBtn);
+            this.Controls.Add(this.GrVarListCheckBox);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form2";
+            this.Opacity = 0.8D;
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "GraphView";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        public ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.CheckedListBox GrVarListCheckBox;
+        private System.Windows.Forms.Button CloseBtn;
+        private System.Windows.Forms.Button StrtBtn;
+        public System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ IboxTestTCP/Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using ZedGraph;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IboxTestTCP
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            for (int i=0; i<IboxTestTCP.Form1.Pages[IboxTestTCP.Form1.ShPgSelCombo.SelectedIndex].VarCount;i++)
+            {
+                GrVarListCheckBox.Items.Add(IboxTestTCP.Form1.Pages[IboxTestTCP.Form1.ShPgSelCombo.SelectedIndex].Variables[i].Name);
+            }
+            GraphPane mypane = zedGraphControl1.GraphPane;
+            //mypane.Title = IboxTestTCP.Form1.Pages[IboxTestTCP.Form1.ShPgSelCombo.SelectedIndex].Name;
+            mypane.Title = null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IboxTestTCP.Form1.ShVarUpdTimer.Enabled = true;
+            button1.Text = Form1.Pages[0].Name;
+        }
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
